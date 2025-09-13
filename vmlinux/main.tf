d12540e8a9e45ddbd928cfdb991094759ca1d033
Olu Olu <oolusegun79@gmail.com>
2025-09-13 00:29:50 -0500

    Complete working script for creating a linux server

--- vmlinux/main.tf
+++ vmlinux/main.tf
@@ -24,3 +24,110 @@ resource "azurerm_resource_group" "rg" {
     environment = "dev"
   }
 }
+
+resource "azurerm_virtual_network" "vnetwork" {
+  name                = "v-network"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+  # dns_servers         = ["10.0.0.4", "10.0.0.5"]
+    tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_subnet" "mysubnet" {
+  name                 = "my-subnet01"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnetwork.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "netgroup" {
+  name                = "my-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_network_security_rule" "networkrule" {
+  name                        = "my-rules"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.netgroup.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "sngassociation" {
+  subnet_id                 = azurerm_subnet.mysubnet.id
+  network_security_group_id = azurerm_network_security_group.netgroup.id
+}
+
+resource "azurerm_public_ip" "mypublicip" {
+  name                = "my-public-ip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_network_interface" "netinterface" {
+  name                = "net-interface-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.mysubnet.id
+    private_ip_address_allocation = "Dynamic"
+#    private_ip_address_id         = azurerm_private_ip.myprivateip.id
+    public_ip_address_id          = azurerm_public_ip.mypublicip.id
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vmlinux" {
+  name                = "vm-my-linux"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "admin123"
+  network_interface_ids = [
+    azurerm_network_interface.netinterface.id
+  ]
+
+  admin_ssh_key {
+    username   = "admin123"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+  tags = {
+    environment = "dev"
+  } 
+}
